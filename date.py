a372f42ba21a43b85f394a16d7c8669ab7806267
Taghwo <taghwomills@gmail.com>
2021-03-20 20:55:48 +0100

    removed large file

--- date.py
+++
@@ -1,7 +0,0 @@
-import datetime
-import time
-
-date = datetime.datetime.strptime("2021-03-19 09:13:00+00:00".replace('+00:00',''),"%Y-%m-%d %H:%M:%S")
-tuple = date.timetuple()
-timestamp = time.mktime(tuple)
-print(time.ctime(timestamp).replace(' ','-'))
