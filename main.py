036cc4907cf559b384619a58a1a652a36c63f48f
Taghwo <taghwomills@gmail.com>
2021-03-20 20:48:05 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import boto3
+import os
+from zipfile import ZipFile
+from dotenv import load_dotenv
+load_dotenv()
+import time
+import shutil
+import datetime
+
+
+def pulls3object():
+
+     s3 = boto3.resource('s3', endpoint_url = 'https://s3.wasabisys.com',aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                      aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                      config=boto3.session.Config(signature_version='s3v4'),
+                      region_name=os.getenv('AWS_REGION')
+                      )
+     bucket = s3.Bucket(os.getenv('AWS_PUBLIC_S3_BUCKET_NAME'))
+     files_payload = []
+     for file in bucket.objects.all():
+         if (file.last_modified).replace(tzinfo = None) > datetime.datetime(2021, 3, 14,tzinfo = None):
+            if('resume' not in file.key):
+                files_payload.append({'file':file.key,'timestamp':file.last_modified})
+                download_template_from_aws(file.key,file.last_modified)
+    #   with open('data.json', 'w', encoding='utf-8') as f:
+    #        json.dump(files_payload, f, indent=4, sort_keys=True, default=str)
+     print('zipping file');
+     root_dir = 'remotefiles'
+     shutil.make_archive(root_dir, 'zip', root_dir)
+
+def download_template_from_aws(s3_file_name,last_modified):
+    s3 = boto3.client('s3',endpoint_url = 'https://s3.wasabisys.com', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                      aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+    try:
+        '''
+        check if directory exist
+        '''
+        root_dir = 'remotefiles'
+
+        date = datetime.datetime.strptime(str(last_modified).replace('+00:00',''),"%Y-%m-%d %H:%M:%S")
+        tuple = date.timetuple()
+        timestamp = time.mktime(tuple)
+
+        human_read_able_time = time.ctime(timestamp).replace(' ','-').replace(':','-')
+
+        sub_dir = f'{root_dir}/{human_read_able_time}/'
+
+        os.mkdir(sub_dir)
+
+        file_path = f'{sub_dir}/{human_read_able_time}.mp3'
+
+        s3.download_file(
+            Bucket=os.getenv('AWS_PUBLIC_S3_BUCKET_NAME'),
+            Key=s3_file_name,
+            Filename=file_path
+        )
+        print('downloaded')
+
+    except Exception as e:
+        print('downloadFileS3@Error')
+        print(e.args)
+pulls3object()
